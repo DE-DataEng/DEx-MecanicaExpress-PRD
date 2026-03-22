3857035e678bdb4db28073d723e81b09d5e74232
Almir J Gomes <46841297+almirjgomes@users.noreply.github.com>
2026-03-22 12:59:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+import os
 import flet as ft
 
 from src.app.app import create_app
 
+
 def main(page: ft.Page) -> None:
-    # print("Hello world!")
     app = create_app(page)
     app.initialize()
 
 
 if __name__ == "__main__":
-    _view = {
-        "desktop": ft.FLET_APP,
-        "webbrowser": ft.WEB_BROWSER,
-        "mobile": ft.FLET_APP
-    }
-    ft.app(target=main, view=_view["webbrowser"])
+    port = int(os.getenv("PORT", "8000"))
+    ft.app(
+        target=main,
+        host="0.0.0.0",
+        port=port,
+    )
